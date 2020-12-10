eb715482467a0216e4d553902a3f434e912bff20
WataruOjima <wataru.ojima20@gmail.com>
2020-12-10 17:13:03 +0900

    (最新版)Issue 6
    メモがコメントアウトになっていませんでした。

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -45,6 +45,6 @@ class CalendarsController < ApplicationController
       days = { :month => (@todays_date + x).month, :date => (@todays_date + x).day, :plans => today_plans, wday: wdays[wday_num]}
       @week_days.push(days)
     end
-    @week_days = [{},{},{},{},{},{}]
+    # @week_days = [{},{},{},{},{},{},{}]
   end
 end
